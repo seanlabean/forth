87ea324d563917d21d95bc03feff04ae34ce525c
Sean Lewis <sean.phys@gmail.com>
2025-04-05 16:10:01 -0400

    *

--- blorth/blorth.fs
+++ blorth/blorth.fs
@@ -76,6 +76,7 @@ variable #token
 ( commands )
 : html	    ." <!DOCTYPE html><head><title>" srcfile TYPE ." </title></head><body>" CR ;
 : /html     ." </body></html>" ;
+: style     ." <style> body { padding: 30px; text-align: absolute center; } </style>" CR ;
 : p	    ." <p>" ;
 : /p	    ." </p>" ;
 : +p	    /p CR p ;
